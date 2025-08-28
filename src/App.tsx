c06b0b102029b068b9d0e6d57a3bd2c89f108d29
CRMwebsolution <crmwebsolution@gmail.com>
2025-08-27 21:18:54 -0400

    Add test console log to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 export default function App() {
+  console.log('App component is rendering - console logging is working!')
+  
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
