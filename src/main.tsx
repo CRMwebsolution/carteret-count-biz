6ac304208ce87e06b1f1a8363d3e46f645f777e4
CRMwebsolution <crmwebsolution@gmail.com>
2025-08-29 19:05:28 -0400

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import Home from './pages/Home'
-import Categories from './pages/Categories'
-import AddBusiness from './pages/AddBusiness'
-import Listing from './pages/Listing'
-import Verify from './pages/Verify'
-import Account from './pages/Account'
-import Admin from './pages/Admin'
-import { AuthProvider } from './providers/AuthProvider'   // ⬅️ add this
-
-const router = createBrowserRouter([
-  { path: '/', element: <App />, children: [
-    { index: true, element: <Home /> },
-    { path: 'categories', element: <Categories /> },
-    { path: 'add', element: <AddBusiness /> },
-    { path: 'verify/:id', element: <Verify /> },
-    { path: 'listing/:id', element: <Listing /> },
-    { path: 'account', element: <Account /> },
-    { path: 'admin', element: <Admin /> },
-  ]}
-])
+import { AuthProvider } from './providers/AuthProvider'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
